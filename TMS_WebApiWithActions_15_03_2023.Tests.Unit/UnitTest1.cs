7c13d29ee082f85f96793476acba902e3abb06ed
XedDevi1 <vadim.presman@gmail.com>
2023-03-15 20:20:29 +0300

    initial

---
+++ TMS_WebApiWithActions_15_03_2023.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace TMS_WebApiWithActions_15_03_2023.Tests.Unit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var actual = 18;
+
+            Assert.Equal(18, actual);
+        }
+    }
+}
---
+++ TMS_WebApiWithActions_15_03_2023/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace TMS_WebApiWithActions_15_03_2023
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
